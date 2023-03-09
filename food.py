03739ce67315e5016bb85b281c5c283f7e6e7b4d
Ahmet <1yigitbilici@gmail.com>
2023-03-09 15:02:17 -0600

    Initialize food

--- food.py
+++ food.py
@@ -1,7 +1,11 @@
 from turtle import Turtle
 import random
 
+# Food class
+
 class Food(Turtle):
+
+    # Create an instance of food with selected styles
     def __init__(self):
         super().__init__()
         self.shape("circle")
@@ -11,6 +15,7 @@ class Food(Turtle):
         self.speed("fastest")
         self.re_position()
 
+    # Re position function that changes the coordinates of the food as game goes on
     def re_position(self):
         rand_x = random.randint(-280, 280)
         rand_y = random.randint(-280, 280)
