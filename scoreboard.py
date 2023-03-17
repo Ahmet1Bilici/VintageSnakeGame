9759f4584f866ea93da53c4cd1645c59817e8e90
Ahmet <1yigitbilici@gmail.com>
2023-03-17 11:52:39 -0500

    Change game_over function to reset function

--- scoreboard.py
+++ scoreboard.py
@@ -7,6 +7,7 @@ class ScoardBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.penup()
         self.hideturtle()
         self.color("#4C4B16")
@@ -14,14 +15,15 @@ class ScoardBoard(Turtle):
         self.update_scoreboard()
 
     def increase_score(self):
-        self.clear()
         self.score += 1
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
